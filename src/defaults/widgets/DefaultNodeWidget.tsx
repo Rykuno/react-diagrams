f35be0b6e9d340a14d2c726bf8faa634ac6bb6e8
Donny Blaine <rykuno@gmail.com>
2019-02-27 11:42:55 -0600

    Update DefaultNodeWidget.tsx

--- src/defaults/widgets/DefaultNodeWidget.tsx
+++ src/defaults/widgets/DefaultNodeWidget.tsx
@@ -30,6 +30,7 @@ export class DefaultNodeWidget extends BaseWidget<DefaultNodeProps, DefaultNodeS
 			<div {...this.getProps()} style={{ background: this.props.node.color }}>
 				<div className={this.bem("__title")}>
 					<div className={this.bem("__name")}>{this.props.node.name}</div>
+					<div className={this.bem("__name")}>{this.props.node.name}</div>
 				</div>
 				<div className={this.bem("__ports")}>
 					<div className={this.bem("__in")}>
